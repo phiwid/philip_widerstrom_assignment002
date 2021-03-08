a84471faa760c0c426d922ec698303b7477a78d0
Philip Widerstrom <Philip.widerstrom@live.se>
2021-03-08 17:31:52 +0100

    from ecplise

--- src/main/java/com/nackademin/philip_widerstrom_assignment002/Main.java
+++ src/main/java/com/nackademin/philip_widerstrom_assignment002/Main.java
@@ -40,7 +40,6 @@ public class Main {
 		System.out.println("SquareArea:  "  + ac.areaOfSqare(valueOne, valueTwo));
 		System.out.println("CircleArea:  "  + ac.areaOfCircle(valueTwo));
 		System.out.println("-------------------------------------:");
-		System.out.println("From Eclipse, virtual box");
 
 		
 		
